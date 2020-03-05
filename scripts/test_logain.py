c5d4b4cdbbdc5da4a6e87e4d2d58b7a88b766970
liangkai123 <281001994@qq.com>
2020-03-05 14:08:04 +0800

    hello

---
+++ scripts/test_logain.py
@@ -0,0 +1,9 @@
+class TestLogain:
+    def test_case1(self):
+        assert 1
+    def test_case2(self):
+        assert 1
+    def test_case3(self):
+        assert 0
+    def test_case4(self):
+        assert 1
